7d737aed23e180fda54d88e55406a91397707ccc
Matthew Fiorella <mfiorell@ucla.edu>
2024-02-11 22:37:27 -0800

    Arduino nano 33

---
+++ Lab4/MyWifi/MyWifi.ino
@@ -0,0 +1,80 @@
+#include <WiFiNINA.h>
+#include "Wifi_Secret.h"
+
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+int status = WL_IDLE_STATUS;             // the Wi-Fi radio's status
+int ledState = LOW;                       //ledState used to set the LED
+unsigned long previousMillisInfo = 0;     //will store last time Wi-Fi information was updated
+unsigned long previousMillisLED = 0;      // will store the last time LED was updated
+const int intervalInfo = 5000;            // interval at which to update the board information
+
+void setup() {
+  // Initialize Serial and wait for port to open
+  Serial.begin(9600);
+  while(!Serial);
+
+  // set the LED as output
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // atempt to connect to Wi-Fi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to network: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connect now, so print out the data:
+  Serial.println("You're connected to the network");
+  Serial.println("-----------------------");
+}
+
+void loop() {
+
+  unsigned long currentMillisInfo = millis();
+  
+  // check if the time after the last update is bigger than the interval
+  if (currentMillisInfo - previousMillisInfo >= intervalInfo) {
+    previousMillisInfo = currentMillisInfo;
+    Serial.println("Board Information: ");
+    // print your board's IP{ address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.print(ip);
+
+    // print your network's SSID:
+    Serial.println();
+    Serial.println("Network Information:");
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print the received signal strength
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI): ");
+    Serial.println(rssi);
+    Serial.println("---------------------");
+  }
+  unsigned long currentMillisLED = millis();
+
+  // measure the signal strength and convert it into a time interval 
+  int intervalLED = WiFi.RSSI() * -10;
+
+  // check if the time after the last blink is bigger than the interval
+  if ( currentMillisLED - previousMillisLED >= intervalLED ) {
+    previousMillisLED = currentMillisLED;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(LED_BUILTIN, ledState);
+  }
+}
---
+++ Lab4/MyWifi/Wifi_Secret.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "mando"
+#define SECRET_PASS "c4AEcHEvR3eu.PqGf*AZ"
